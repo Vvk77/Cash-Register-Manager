d233cfe3f06305870f6e565483d12befe64dd111
Vvk77 <vvk.yadav12@gmail.com>
2022-10-27 18:28:36 +0530

    update:finalproject

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const billAmount = document.querySelector("#bill-amount");
 const checkBtn = document.querySelector("#check-btn");
 const cashGiven = document.querySelector("#cash-given");
 const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes =[2000,500,100,20,10,5,1];
 
@@ -9,7 +10,7 @@ checkBtn.addEventListener("click", function validateBillAndCashAmout() {
     hideMessage();
 
     if (billAmount.value > 0) {
-        if(cashGiven.value >= billAmount.value){
+        if(Number(cashGiven.value) >= Number(billAmount.value)){
             const amountToBeReturned = cashGiven.value - billAmount.value;
 calculateChange(amountToBeReturned);
 
@@ -35,6 +36,19 @@ calculateChange(amountToBeReturned);
 
 });
 function calculateChange(amountToBeReturned) {
+    for (let i=0; i< availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i] 
+            // noOfNotes[i].innerText = numberOfNotes
+
+
+        );
+
+amountToBeReturned %= availableNotes[i];
+noOfNotes[i].innerText = numberOfNotes
+
+
+
+    }
 
 }
 
